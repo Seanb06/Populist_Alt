c6e3ce4b7256fcc29452d975eb4c74fad7bfc6f0
Carrie Kengle <carriesteady@gmail.com>
2016-06-21 17:23:47 -0400

    MainNav updates

--- populist/src/PopulistApp/client/views/Shared/MainNav.jsx
+++ populist/src/PopulistApp/client/views/Shared/MainNav.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
 import { Link, browserHistory } from 'react-router'
-import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
-export default function MainNav() {
-  return ( 
-    
-    <div className=""> 
-    Top Nav <AppBar title="My AppBar" />
-    </div>
-  )
+
+var toolbarStyle = {
+  background: 'rgba(215,215,215,0.34)'
+};
+
+export default class MainNav extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 1,
+    };
+  }
+
+  handleChange = (event, index, value) => this.setState({value});
+
+  render() {
+    return ( 
+      <Toolbar style={toolbarStyle}>
+        <ToolbarGroup firstChild={true}>
+          <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+            <MenuItem value={1} primaryText="Browse" />
+            <MenuItem value={2} primaryText="Category" />
+            <MenuItem value={3} primaryText="Category" />
+            <MenuItem value={4} primaryText="Category" />
+            <MenuItem value={5} primaryText="Category" />
+            <MenuItem value={6} primaryText="Category" />
+            <MenuItem value={7} primaryText="Category" />
+          </DropDownMenu>
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <ToolbarSeparator />
+          <RaisedButton label="Create a profile" primary={true} />
+          <RaisedButton label="Sign in" secondary={true} />
+        </ToolbarGroup>
+      </Toolbar>
+    )
+  }
 }
