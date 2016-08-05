c667816952c6c44aa2d2e0c038f776d23ed8b591
Carrie Kengle <carriesteady@gmail.com>
2016-08-05 12:10:24 -0400

    meta viewport

--- populist/src/PopulistApp/client/views/ComingSoon/ComingSoon_component.jsx
+++ populist/src/PopulistApp/client/views/ComingSoon/ComingSoon_component.jsx
@@ -11,7 +11,7 @@ const ComingSoon = () => (
 
         <div className="btn-holder">
             <a href="https://www.facebook.com/populists/" className="facebook" target="_blank">Follow us on Facebook</a>
-            <a href="https://twitter.com/PopulistTweet" className="twitter" target="_blank">Follow us on Twitter</a>
+            <a href="https://twitter.com/PopulistTweets" className="twitter" target="_blank">Follow us on Twitter</a>
         </div>
 
         <span>Coming soon!</span>
